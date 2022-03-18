23bcfa71172788d4bb6b9b3cb5c1e4ee2a904b94
washwor1 <washwor1@vols.utk.edu>
2022-03-18 13:14:49 -0400

    finished

--- epic.cpp
+++ epic.cpp
@@ -3,42 +3,90 @@
 #include <iostream>
 #include <string>
 #include <map>
-#include <moss.h>
+#include <sstream>
 
 using namespace std;
 
 //function that decodes the moss speak
-int decode(string &input);
+int decode(string &input,const map<char,char> &convert);
 
 //function that encodes the moss speak
-int encode(string &input);
+int encode(string &input,const map<char,char> &convert);
 
 //function that prints out the usage
 void help();
 
 int main() {
-    map<int,char> conversions;
-    bool exit = false;
+    map<char,char> encodev;
+    map<char,char> decodev;
     string input;
 
-    conversions.insert(make_pair(0,'*'));
-    conversions.insert(make_pair(1,'.'));
-    conversions.insert(make_pair(2,','));
-    conversions.insert(make_pair(3,'\''));
-    conversions.insert(make_pair(4,'\"'));
-    conversions.insert(make_pair(5,':'));
-    conversions.insert(make_pair(6,'#'));
-    conversions.insert(make_pair(7,'-'));
+    encodev.insert(make_pair('0','*'));
+    encodev.insert(make_pair('1','.'));
+    encodev.insert(make_pair('2',','));
+    encodev.insert(make_pair('3','\''));
+    encodev.insert(make_pair('4','\"'));
+    encodev.insert(make_pair('5',':'));
+    encodev.insert(make_pair('6','#'));
+    encodev.insert(make_pair('7','-'));
 
+    decodev.insert(make_pair('*','0'));
+    decodev.insert(make_pair('.','1'));
+    decodev.insert(make_pair(',','2'));
+    decodev.insert(make_pair('\'','3'));
+    decodev.insert(make_pair('\"','4'));
+    decodev.insert(make_pair(':','5'));
+    decodev.insert(make_pair('#','6'));
+    decodev.insert(make_pair('-','7'));
+
+    cout<<'\n';
     help();
 
-    while(!exit && getline(cin,input)) {
-        if(input[0] == 'e') encode(input);
-        else if (input[0] == 'd') decode(input);
+    while(getline(cin,input)) {
+        if(input[0] == 'e') encode(input,encodev);
+        else if (input[0] == 'd') decode(input,decodev);
         else help();
     }
 }
 
 void help() {
-    cout << "Usage:[d,e] \"String of text\"\n\ne - encode\nd - decode\n";
+    cout << "Usage:[d,e] \"String of text\"\n\ne - encode\nd - decode\n\n";
+}
+
+int encode(string &input,const map<char,char> &convert) {
+    string output,tmp = "";
+    stringstream ss;
+    ss.fill('0');
+    ss.width(3);
+
+    for (int i = 2;i<input.size();i++) {
+        ss << oct << int(input[i]);
+        tmp += ss.str();
+        ss.str("");
+        ss.fill('0');
+        ss.width(3);
+    }
+    
+    for (int i = 0; i < tmp.size(); i++) {
+        output += convert.find(tmp[i])->second;
+    }
+    cout<<output<<'\n';
+    return 0;
+}
+
+int decode(string &input,const map<char,char> &convert) {
+    string output, tmp = "";
+    int bruh;
+
+    for(int i = 2; i<input.size();i+=3) {
+        tmp+=convert.find(input[i])->second;
+        tmp+=convert.find(input[i+1])->second;
+        tmp+=convert.find(input[i+2])->second;
+        bruh = stoi(tmp,0,8);
+        output+=bruh;
+        tmp="";
+    }
+    cout << output << '\n';
+
+    return 0;
 }
